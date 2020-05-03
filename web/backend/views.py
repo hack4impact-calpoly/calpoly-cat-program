adf7c8bf79c62dd8f34757db53aab4ae8a9e9c88
Ethan Zimbelman <itszimbo@gmail.com>
2020-05-03 02:50:21 -0500

    cat update

--- web/backend/views.py
+++ web/backend/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
 
 from backend.serializers import *
-from directory.models import Client
+from directory.models import Cat 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -14,11 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
-
-class ClientViewSet(viewsets.ModelViewSet):
-    """
-    Endpoint for adding and viewing Clients
-    """
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
     # permission_classes = [permissions.IsAuthenticated]
--- web/directory/views.py
+++ web/directory/views.py
@@ -2,40 +2,50 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect 
 
-from .forms import ClientForm
-from .models import Client
+from .forms import IntakeForm 
+from .models import Cat
 
-# Create your views here.
 def index(request):
-    clients = Client.objects.all()
-    return render(request, 'client_list.html', {'clients': clients})
+    cats = Cat.objects.all()
+    return render(request, 'landing.html', {'cats': cats})
 
-def client(request):
-    client_id = request.GET.get('id')
-    print(client_id)
-    client = Client.objects.get(id=client_id)
-    return render(request, 'client.html', {'client': client})
+def cat_profile(request):
+    cat_id = request.GET.get('id')
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cat_profile.html', {'cat': cat})
 
 def help(request):
     return render(request, 'help.html')
 
-def add_client(request):
-   if request.method == 'POST':
-      form = ClientForm(request.POST)
-      if form.is_valid():
-         data = form.cleaned_data
-         client = Client(first_name = data.get('first_name'),
-                         last_name = data.get('last_name'),
-                         birthday = data.get('birthday'),
-                         location = data.get('location'))
-         client.save()
+def intake_form(request):
+    if request.method == 'POST':
+        form = IntakeForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            cat = Cat(
+                    name = data.get('name'),
+                    gender = data.get('gender'),
+                    age = data.get('age'),
+                    description = data.get('description'),
+                    breed = data.get('breed'),
+                    itype = data.get('itype'),
+                    status = data.get('status'),
+                    arrival_date = data.get('arrival_date'),
+                    arrival_details = data.get('arrival_details'),
+                    medical_history = data.get('medical_history'),
+                    vaccinations = data.get('vaccinations'),
+                    is_microchipped = data.get('is_microchipped'),
+                    flea_control_date = data.get('flea_control_date'),
+                    deworming_date = data.get('deworming_date'),
+                    fiv_felv_date = data.get('fiv_felv_date'),
+                    special_needs = data.get('special_needs'),
+                    personality = data.get('personality'),
+                    more_personality = data.get('more_personality'),
+                    comments = data.get('comments'),
+                    personal_exp = data.get('personal_exp'))
 
-         return HttpResponseRedirect('view_clients')
-   else:
-      form = ClientForm()
-
-   return render(request, 'add_client.html', {'form': form})
-
-def view_clients(request):
-   clients = Client.objects.all()
-   return render(request, 'view_clients.html', {'clients': clients})
+            cat.save()
+        return HttpResponseRedirect('/cat/?id=' + cat.id)
+    else:
+        form = IntakeForm()
+        return render(request, 'intake.html', {'form': form})
