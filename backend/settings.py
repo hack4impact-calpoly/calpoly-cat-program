9845dc1a95e6f3cf9a3b2b58ddb71bf2c3024728
Ethan Zimbelman <itszimbo@gmail.com>
2020-07-08 00:58:54 -0500

    wsgi for static

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'directory/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # media storage
