5c09bdf282da0fcb2c36dece3194c9f2ba1cea37
Ethan Zimbelman <itszimbo@gmail.com>
2020-07-08 00:08:10 -0500

    another go at serving static files :)

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/directory/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
