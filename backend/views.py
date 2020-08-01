a7e431126643573097c77b36d47b8faeb0fe8775
Ethan Zimbelman <itszimbo@gmail.com>
2020-08-01 14:20:43 -0500

    volunteer training added and vet appts restricted

--- backend/views.py
+++ backend/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from backend.serializers import *
-from directory.models import Cat 
-from directory.models import Document 
+from directory.models import Cat
+from directory.models import Document
 from directory.models import Event
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,7 +27,7 @@ class CatViewSet(viewsets.ModelViewSet):
         return queryset
 
 class DocumentViewSet(viewsets.ModelViewSet):
-    serializer_class = DocumentSerializer 
+    serializer_class = DocumentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
@@ -36,9 +36,9 @@ class DocumentViewSet(viewsets.ModelViewSet):
         if cat_id is not None:
             queryset = queryset.filter(cat_id=cat_id)
         return queryset
-        
+
 class PhotoViewSet(viewsets.ModelViewSet):
-    serializer_class = PhotoSerializer 
+    serializer_class = PhotoSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
@@ -47,13 +47,17 @@ class PhotoViewSet(viewsets.ModelViewSet):
         if cat_id is not None:
             queryset = queryset.filter(cat_id=cat_id)
         return queryset
-        
+
 class EventViewSet(viewsets.ModelViewSet):
-    serializer_class = EventSerializer 
+    serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = Event.objects.filter(hidden=False)
+
+        if not self.request.user.is_superuser:
+            queryset = queryset.exclude(event_type='vet')
+
         cat_id = self.request.query_params.get('cat_id', None)
         if cat_id is not None:
             queryset = queryset.filter(cat_id=cat_id)
