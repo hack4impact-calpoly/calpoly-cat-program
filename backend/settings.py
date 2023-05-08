34ef1114e105d03b11e110ac18a5390f18870e9a
AnnaReid22 <anna.reid@live.com>
2023-05-08 16:23:53 -0700

    settings change

--- backend/settings.py
+++ backend/settings.py
@@ -67,11 +67,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 if config('ISPROD', default=True, cast=bool):
     DATABASES['default'] = dj_database_url.config()
